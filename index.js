957219c787d996fa85920472ceed7aca9c4f7841
97Boivob97 <125070916+97Boivob97@users.noreply.github.com>
2024-05-28 04:10:25 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+var a = document.getElementById("para1");
+var b = document.getElementsByTagName("img")[0];
+function myMessage1(){
+    a.innerHTML = "You have clicked button 1 for lion";
+    b.src = "lion.JPG";
+    
+}
+
+function myMessage2(){
+    a.innerHTML = "You have clicked button 2 for tiger";
+    b.src = "tiger.jpg";
+}
+
+
+
